42934e83eec77efafd41a337df0e143813d13d68
Pietari Kaskela <pietari.kaskela@aalto.fi>
2019-01-14 21:10:32 +0200

    ready v1

--- laskaritbot.py
+++ laskaritbot.py
@@ -1,34 +1,52 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""Basic example for a bot that uses inline keyboards.
-# This program is dedicated to the public domain under the CC0 license.
-"""
+from datetime import time, tzinfo
 import logging
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+
+class GMT2(tzinfo):
+    def utcoffset(self, dt):
+        return timedelta(hours=2)
+    def dst(self, dt):
+        return timedelta(0)
+    def tzname(self,dt):
+        return "Europe/Helsinki"
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def start(bot, update, job_queue,):
-    job_queue.run_daily(kysyLaskarit,, [0, 1, 2, 3, 4, 5, 6], context=None, name=None) 
+def start(bot, update, job_queue):
+    t = time(21, 9, 45)
+    logger.info(t.isoformat())
+    job_queue.run_daily(kysyLaskarit, t, (0, 1, 2, 3, 4, 5, 6), context=update.message.chat_id, name=None) 
+    update.message.reply_text('Laskarit ajoitus aloitettu about 20')
     
-def kysyLaskarit(bot, update):
+def kysyLaskarit(bot, job):
     keyboard = [[InlineKeyboardButton("Joo", callback_data='1'),
                  InlineKeyboardButton("Ei :(", callback_data='2')]]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Oletko tehnyt laskarit???', reply_markup=reply_markup)
+    bot.send_message(job.context, 'Oletko tehnyt laskarit???', reply_markup=reply_markup)
 
+def clearJobs(chat_data):
+    if 'job' in chat_data:
+        job = chat_data['job']
+        job.schedule_removal()
+        del chat_data['job']
 
-def button(bot, update):
+def button(bot, update, chat_data, job_queue):
     query = update.callback_query
-
-    bot.edit_message_text(text="Selected option: {}".format(query.data),
-                          chat_id=query.message.chat_id,
-                          message_id=query.message.message_id)
+    logger.info('v ' + str(query.data))
+    if query.data == '1':
+        bot.send_message(text='Hyvä!', chat_id=query.message.chat_id,)
+        clearJobs(chat_data)
+    else:
+        bot.send_message(chat_id=query.message.chat_id, text=':(')
+        job = job_queue.run_once(kysyLaskarit, 10*60, context=query.message.chat_id)
+        clearJobs(chat_data)
+        chat_data['job'] = job
+    
 
 
 def help(bot, update):
@@ -44,11 +62,11 @@ def main():
     # Create the Updater and pass it your bot's token.
     updater = Updater("")
 
-    updater.dispatcher.add_handler(CommandHandler('start', start, 
+    updater.dispatcher.add_handler(CommandHandler('aloita', start, 
                                   pass_job_queue=True))
     updater.dispatcher.add_handler(CallbackQueryHandler(button,
-                                  pass_job_queue=True))
-    updater.dispatcher.add_handler(CommandHandler('help', help))
+                                  pass_job_queue=True, pass_chat_data=True))
+    updater.dispatcher.add_handler(CommandHandler('apua', help))
     updater.dispatcher.add_error_handler(error)
 
     # Start the Bot
